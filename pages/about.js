d0a76a7e6b7f722b52fa59314b2a4ffad312ad7c
tuongpixta <tuong.nguyendinh@pixta.co.jp>
2021-12-10 15:55:52 +0700

    first commit

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
+import Layout from "../components/layout";
+export default function About() {
+  return (
+    <Layout children={undefined} home={undefined}>
+      <Head children={undefined}>
+        <title>About</title>
+      </Head>
+      <h1 className="text-gray-500">About</h1>
+      <h2>
+        <Image src="/images/profile.jpg" height={144} width={144} />
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
+  );
+}
